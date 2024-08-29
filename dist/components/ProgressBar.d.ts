fe68829aab9c2cc0df584032eed31aa449a12967
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-08-30 00:55:28 +0545

    feat: addgitignore

--- dist/components/ProgressBar.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as React } from 'react';
-interface ProgressBarProps {
-    progress: number;
-    barColor?: string;
-    backgroundColor?: string;
-    height?: string;
-}
-declare const ProgressBar: React.FC<ProgressBarProps>;
-export default ProgressBar;
